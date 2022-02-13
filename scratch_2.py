ff9a53ab4041b0c9d00c39adac46f92c411c8cb6
Adriana <hrl.davidson@gmail.com>
2022-02-13 22:39:31 +0100

    update

--- scratch_2.py
+++ scratch_2.py
@@ -2,16 +2,16 @@
 mylist = ["banana", "cherry", "apple"]
 print(mylist)
 
-mylist2 = list() #empty List
+mylist2 = list()   # empty List
 print(mylist2)
 
-mylist2 = [5, True, "apple", "apple"] #doppelte elemente
+mylist2 = [5, True, "apple", "apple"]  # doppelte elemente
 print(mylist2)
 
 item = mylist[1]
 print(item)
 
-for i in mylist: #alle elemente
+for i in mylist:   # alle elemente
     print(i)
 
 # checking ob gibts ein besonderen Element
@@ -27,4 +27,62 @@ print(len(mylist))
 mylist.append("lemon")
 print(mylist)
 
-#addieren zu einem spezifischen Platz
+# addieren zu einem spezifischen Platz
+mylist.insert(1, "blueberry")
+print(mylist)
+
+# letzten item löschen
+item = mylist.pop()
+print(item)
+print(mylist)
+
+# spezifische Element löschen
+item = mylist.remove("cherry")
+print(mylist)
+
+# reverse the list
+item = mylist.reverse()
+print(mylist)
+
+# elemente sortieren
+mylist3 = [4, 2, 9, 53]
+print(mylist3)
+item = mylist3.sort()
+print(mylist3)
+
+# neue organisierte Liste erzeugen
+new_list = sorted(mylist3)
+print(new_list)
+
+# concat Liste
+new_list2 = new_list + mylist
+print(new_list2)
+
+# alle elemente löschen
+item = mylist.clear()
+print(mylist)
+
+# slicing Method
+dieliste = [1, 2, 3, 4, 5]
+dieliste2 = dieliste[1:5]  # falls [:5] -> fängt von Anfang an, falls [1:] geht zu ende
+print(dieliste2)
+
+dieliste = [1, 2, 3, 4, 5]
+dieliste2 = dieliste[::2] # jeden zweiten Element wird ausgegeben
+dieliste2 = dieliste[::-1] # die Liste umkehren
+print(dieliste2)
+
+# Liste kopieren und in die kopierte etwas addieren (originale bleibt wie sie ist)
+list_org = ["banana", "cherry", "apple"]
+# list_cpy = list_org.copy() # erste Option
+# list_cpy = list(list_org) # zweite Option
+list_cpy = list_org[:] # dritte Option
+list_cpy.append("lemon")
+print(list_cpy)
+print(list_org)
+
+# list comprehension - create list out of list
+a = [1, 2, 3]
+b = [i*i for i in a]
+print(a)
+print(b)
